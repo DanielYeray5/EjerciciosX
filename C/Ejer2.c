58b49a87136fabff76f0c0ace908aecf539557f5
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-12 21:43:08 -0600

    Dia 11

--- C/Ejer2.c
+++ C/Ejer2.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
-int main(){
-    
+
+int main()
+{
+    int anio;
+
+    printf( "\n   Introduzca un anio: ", 164 );
+    scanf( "%d", &anio );
+
+    if ( anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0 )
+        printf( "\n   ES BISIESTO" );
+    else
+        printf( "\n   NO ES BISIESTO" );
+
+    return 0;
 }
