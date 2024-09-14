47632a7a74cd416e336f30c405d48f780d8ce989
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-14 16:29:52 -0600

    Dia 12

--- C/Ejer3.c
+++ C/Ejer3.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(){
-    
-}
+    int main()
+    {
+        int numero;
+
+        printf("\n   ");
+
+        for (numero = 1; numero <= 5; numero++)
+        {
+            printf("%d %d ", numero, -numero);
+        }
+
+        return 0;
+    }
