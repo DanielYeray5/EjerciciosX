126402db7540fe2d9cd8d2f9047932e144033ee3
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-18 19:26:08 -0600

    Dia 16

--- C/Ejer6.c
+++ C/Ejer6.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
-int main(){
-    
+
+int main()
+{
+    int acumulador, contador, n1, n2;
+
+    printf( "\n   Introduzca primer n%cmero (entero): ", 163 );
+    scanf( "%d", &n1 );
+    printf( "\n   Introduzca segundo n%cmero (entero): ", 163 );
+    scanf( "%d", &n2 );
+
+    if ( n1 >= 0 && n2 >= 0 )
+    {
+        acumulador = 0;
+
+        if ( n2 != 0 )
+        {
+            contador = 1;
+            while ( contador <= n1 )
+            {
+                acumulador += n2;
+                contador++;
+            }
+        }
+
+        printf( "\n   %d * %d = %d", n1, n2, acumulador );
+
+    }
+    else
+        printf( "\n   ERROR: Ambos n%cmeros deben ser mayores o iguales que cero.", 163 );
+
+    return 0;
 }
